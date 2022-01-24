c21dcd108fb953523a876cc3822cfc97afc11d8a
Riley B <ribru17@hotmail.com>
2022-01-24 13:59:06 -0800

    initial

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+    useEffect(() => {
+        // fetch('/api/test')
+        // console.log("hi");
+        
+    }, [])
+
+    async function submitTest(e: React.FormEvent) {
+        e.preventDefault()
+        let formData = new FormData()
+        formData.append('myKey', 'myValue')
+        console.log("hi");
+        
+        const postData = {
+            method: 'POST',
+            headers: {
+                // 'Accept': 'application/json',
+                // 'Content-Type': 'multipart/form-data'
+                //TODO: formidable for multipart form data(?)
+                'Content-Type': 'application/json'
+            },
+            // body: formData
+            body: JSON.stringify({"MYDATA": "HI"})
+
+        }
+        await fetch('/api/test', postData)
+    }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +46,14 @@ function App() {
         >
           Learn React
         </a>
+        <iframe title="labelimg" name="labelimg" style={{display: "none"}}></iframe>
+        <form method="POST" encType="multipart/form-data" action="/uploadimg" target="labelimg">
+            <input type="file" name="labelimg" id="labelimg" />
+            <input type="submit" id="imageSubmit" />
+        </form>
+        <form onSubmit={submitTest}>
+            <input type="submit" />
+        </form>
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,5 +1,41 @@
-import cors from 'coors';
-import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import cors from 'cors';
+import mongodb from 'mongodb'
+import express from 'express'
+import bodyParser from 'body-parser'
 
-const express = require('express');
+dotenv.config()
+
+const MongoClient = mongodb.MongoClient
+const client = new MongoClient(process.env.MONGO_URI, {
+    sslKey: process.env.CRED_PATH,
+    sslCert: process.env.CRED_PATH
+},
+{ useUnifiedTopology: true }, { useNewUrlParser: true })
+const PORT = process.env.PORT || 8000
+
 const app = express();
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(express.urlencoded({ extended: true }))
+// app.use(express.bodyParser())
+
+client.connect(err => {
+    if (err) throw err
+    console.log("db connected");
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+})
+
+app.post('/uploadimg', (req, res) => {
+    console.log("received");
+    res.render("HI")
+})
+
+app.post('/api/test', (req, res) => {
+    res.send({ express: "APP IS CONNECTED" })
+    console.log(req.body);
+})
