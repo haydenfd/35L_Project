fcd61c403914f0f08bcdbdd959ed75add9f46807
Ryan Jackson <10ryanjackson10@gmail.com>
2022-02-04 13:41:09 -0800

    main page display with hardcoded data

--- client/src/Components/MainPage/MainPage.tsx
+++ client/src/Components/MainPage/MainPage.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import SubNavbar from "./SubNavbar/SubNavbar";
+import MainFeed from './MainFeed/MainFeed';
 
 function MainPage() {
 
     return (
         <div className="body">
             <SubNavbar />
+            <MainFeed />
         </div>
     )
 }
