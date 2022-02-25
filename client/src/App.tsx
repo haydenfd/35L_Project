91d8307b942678dcdc40906b4a8119005e984141
Richard Li <richli@ucla.edu>
2022-02-24 23:47:26 -0800

    fixed import Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Navbar from './Components/Navbar'
+import Navbar from './Components/Navbar/Navbar'
 // import Middleware from './Components/Middleware/Middleware';
 // import { userObject, postObject } from './Components/Middleware/Middlewaretypes'
 
