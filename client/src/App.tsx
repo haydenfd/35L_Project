99b4c41908d9f1cdebb81775e5e34af1b803703f
Richard Li <richli@ucla.edu>
2022-02-26 22:54:46 -0800

    setting up Form for user to upload data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,17 @@ import Navbar from './Components/Navbar/Navbar'
 // import Middleware from './Components/Middleware/Middleware';
 // import { userObject, postObject } from './Components/Middleware/Middlewaretypes'
 
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Form from './Components/Form/form';
+
+
 function App() {
   // Test()
   return (
     <div className="app">
       <Navbar /> 
+      
     </div>
   );
 }
