d413c6e70bfd11f7edab5f5f06b494f395b4d5f0
Richard Li <richli@ucla.edu>
2022-03-04 00:34:31 -0800

    added reducer for update

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -10,3 +10,12 @@ export const updatePost = (id, post) => async (dispatch) => {
 }
 */
 
+export const reducer = (state, action) => {
+    if (action.type = "UPDATE") {
+        return {
+            ...state,
+            followers:action.payload.followers,
+            following:action.payload.following,
+        }
+    }
+}
