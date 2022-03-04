84eaf414c738095793c0ab71067f8bb9d528f992
haydenfd <haydenfds@gmail.com>
2022-03-04 15:38:07 -0800

    Added event handlers to Login.js Option for Forgotten Password added, functionality not added yet global.js added, to store generic error messages for runtime frontend error checks

--- client/src/Components/Modals/Login.tsx
+++ client/src/Components/Modals/Login.tsx
@@ -1,26 +1,64 @@
 import React from 'react'
 import './index.css'
+import { useState } from 'react'
+import { errors } from './global'
 
-export default function Login(props:any) {
+export default function Login(props: any) {
+  
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleEmailChange = (e: any) => { 
+    setEmail(e.target.value)
+  }
+
+  const handlePasswordChange = (e:any) => { 
+    setPassword(e.target.value)
+    }
+  
+  const handleSubmit = (e:any) => { 
+    e.preventDefault()
+    const data = {
+      email: email, 
+      password: password,
+    }
+
+    console.log(data)
+
+    auth()
+  }
+
+  const auth = () => {
+
+     const pattern = new RegExp("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$");
+
+    if (password.length <= 7) { 
+      document.getElementById('error')!.style.display = 'block';
+      document.getElementById('error')!.innerHTML = errors[4];
+      return false;
+    }
+  }
+
   return (
     <>
       <h2>Login</h2>
       <div>
         <form
-          onSubmit={e => {
-            alert("Sign up!")
-            e.preventDefault()
-          }}
+          onSubmit={handleSubmit}
         >
-          <input id='email' type='email' placeholder='Email address' className='input' />
-          <input id='password' type='password' placeholder='Password' className='input'/>
+          <input id='email' type='email' placeholder='Email address' className='input' onChange={handleEmailChange}  />
+          <input id='password' type='password' placeholder='Password' className='input' onChange={handlePasswordChange} />
           <button className='btn'>Log in</button>
         </form>
       </div>
-      <p>
-        Don't have an account yet?{" "}
+         <p>
         <a href='javascript:;' onClick={() => props.showSignup()}>
-          Sign up
+          I forgot my password
+        </a>
+      </p>
+      <p>
+        <a href='javascript:;' onClick={() => props.showSignup()}>
+          I don't have an account
         </a>
       </p>
     </>
--- client/src/Components/Modals/Register.tsx
+++ client/src/Components/Modals/Register.tsx
@@ -1,20 +1,14 @@
 import React from 'react'
 import './index.css'
 import { useState } from 'react'
+import { errors } from './global'
 
 export default function Register(props:any) {
-
-
 
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [confirmPassword, setConfirmPassword] = useState("") 
 
-  //move more functionality into reset if needed
-  const resetPasswords = () => { 
-      setPassword("");
-      setConfirmPassword("");
-  }
   const handleEmailChange = (e: any) => { 
     setEmail(e.target.value)
   }
@@ -41,33 +35,49 @@ export default function Register(props:any) {
   }
 
   const auth = () => {
+
+     const pattern = new RegExp("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$");
+
+    if (password.length <= 7) { 
+      document.getElementById('error')!.style.display = 'block';
+      document.getElementById('error')!.innerHTML = errors[4];
+      return false;
+    }
+
+  //  else if (pattern.test(password)) { 
+  //     document.getElementById('error')!.style.display = 'block';
+  //     document.getElementById('error')!.innerHTML = errors[5];
+  //     resetPasswords();
+  //     return false;
+  //    }
+
+  else if (password !== confirmPassword) { 
+      document.getElementById('error')!.style.display = 'block';
+      document.getElementById('error')!.innerHTML = errors[2];
+      return false;
+    }
   }
 
   return (
     <>
       <h2>Sign up</h2>
       <div>
-        <form
-          onSubmit={e => {
-            alert("Log in!")
-            e.preventDefault()
-          }}
-        >
-          <input id='email' type='email' placeholder='Email address' className='input' />
-          <input id='password' type='password' placeholder='Password' className='input'/>
+        <form onSubmit={ handleSubmit} >
+          <input id='email' type='email' placeholder='Email address' className='input' onChange={handleEmailChange} />
+          <input id='password' type='password' placeholder='Password' className='input' onChange={handlePasswordChange}/>
           <input
             id='passwordconfirm'
             type='password'
             placeholder='Enter password again'
             className='input'
+            onChange={handleConfirmPasswordChange}
           />
           <button className='btn'>Sign up</button>
         </form>
       </div>
       <p>
-        Already have an account?{" "}
         <a href='javascript:;' onClick={() => props.showLogin()}>
-          Log in
+          I already have an account
         </a>
       </p>
     </>
