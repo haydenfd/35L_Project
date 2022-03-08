6976b1428f4c2f4b6d8d4918801f75684053745d
haydenfd <haydenfds@gmail.com>
2022-03-07 18:48:01 -0800

    Added upload.css and more layering to Upload Formatting of Upload.tsx Functionality not yet complete Rudimentary UI, coloring

--- client/src/Components/ListingPage/Upload.tsx
+++ client/src/Components/ListingPage/Upload.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import React from 'react'
+import './upload.css'
 
 export default function Upload() {
 
@@ -15,31 +16,48 @@ export default function Upload() {
 
 
   return (
-    <div>
-      <title>Add A New Listing</title>
-  <form>
-        <label>Address</label>
-        <input required placeholder="Enter Address"/>
-        <label>Price</label>
-        <input required placeholder="Enter Price" />
-        <label>Distance To Campus</label>
-        <input required placeholder="Enter Distance" />
-        <label>Rent By Date</label>
-        <input required placeholder="Enter Last Date" />
-        <label>Seller</label>
-        <input required placeholder="Seller Name" />
-        <label>Amenities</label>
-        <input required placeholder="Amenities" />
-        <label>Bedrooms</label>
-        <input required placeholder="No. Of Bedrooms" />  
-        <label>Bathrooms</label>
-        <input required placeholder="No. Of Bathrooms" /> 
-        <label>Pictures</label>
-        <input required placeholder="Add Image Here" />  
-
-      <button> Submit </button>
+    <div className='form-box'>
+      <h1>Add A Listing</h1>
+      <form className='form'>
+        <div className='input-div'>
+        <label htmlFor="Address" >Address</label>
+          <input required placeholder="Enter Address" id="Address" />
+        </div>
+        <div className='input-div'>
+        <label htmlFor="Price">Price</label>
+          <input required placeholder="Enter Price" id="Price" />
+        </div>
+        <div className='input-div'>
+        <label htmlFor="Distance">Distance To Campus   </label>
+          <input required placeholder="Enter Distance" id="Distance" />
+        </div>
+        <div className='input-div'>
+        <label htmlFor="RentBy">Rent By Date </label>
+          <input required placeholder="Enter Last Date" id="RentBy" />
+        </div>
+        <div className='input-div'>
+        <label htmlFor="Seller">Seller</label>
+          <input required placeholder="Seller Name" id="Seller" />
+        </div>
+        <div className='input-div'>
+        <label htmlFor="Amenities">Amenities</label>
+          <input required placeholder="Amenities" id="Amenities" />
+        </div>
+        <div className='input-div'>
+        <label htmlFor="Bedrooms">Bedrooms</label>
+          <input required placeholder="No. Of Bedrooms" id="Bedrooms" /> 
+        </div>
+        <div className='input-div'>
+        <label htmlFor="Bathrooms">Bathrooms</label>
+          <input required placeholder="No. Of Bathrooms" id="Bathrooms" /> 
+        </div>
+        <div className='input-div'>
+        <label htmlFor="Pictures">Pictures</label>
+          <input required placeholder="Add Image Here" id="Pictures" /> 
+        </div>
+       <input type="submit" value="Submit" className='btn'/>
       </form>
-  </div>
+    </div>
   )
 
 }
